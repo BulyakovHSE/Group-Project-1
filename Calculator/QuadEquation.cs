05dad65725673f6ae0c742917008a11f2b30899c
Буляков Данил Радисович <drbulyakov@edu.hse.ru>
2017-06-10 13:14:34 +0500

    Added QuadEquation

---
+++ Calculator/QuadEquation.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+namespace Calculator
+{
+    public class QuadEquation
+    {
+        public Complex A { get; set; }
+
+        public Complex B { get; set; }
+
+        public Complex C { get; set; }
+
+        public Complex D
+        {
+            get
+            {
+                return B * B - 4 * A * C;
+            }
+        }
+
+        public Complex X1
+        {
+            get
+            {
+                return (-B + Complex.Pow(D, 0.5)) / 2;
+            }
+        }
+
+        public Complex X2
+        {
+            get
+            {
+                return (-B - Complex.Pow(D, 0.5)) / 2;
+            }
+        }
+
+        public QuadEquation(Complex a, Complex b, Complex c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
